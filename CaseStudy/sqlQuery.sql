e8b2499e9d687fb95546f6483eb7159dbb106715
gjrgubatan <122078684+gjrgubatan@users.noreply.github.com>
2023-01-06 17:58:52 +0800

    Update sqlQuery.sql

--- CaseStudy/sqlQuery.sql
+++ CaseStudy/sqlQuery.sql
@@ -5,5 +5,4 @@ use CaseStudy;
 /*Please run the spring boot app first, then stop. This is to generate the database and tables. Then please run the lines below,
 to insert an admin account for it to pass the login system. Thank you.*/
 INSERT INTO admin VALUES (1, "admin@gmail.com", "Admin", "Root", "password");
-ALTER TABLE employees
-DROP COLUMN email;
+
